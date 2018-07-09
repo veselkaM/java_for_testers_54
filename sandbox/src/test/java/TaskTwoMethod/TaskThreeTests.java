b5654b8e32d392b8e7fe34ab97031976ca3033cf
Daria <37508094+veselkaM@users.noreply.github.com>
2018-07-09 22:40:37 +0300

    Подключен к модулю sandbox тестовый фреймворк TestNG. Создано несколько тестов для класса Point.

---
+++ sandbox/src/test/java/TaskTwoMethod/TaskThreeTests.java
@@ -0,0 +1,31 @@
+package TaskTwoMethod;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TaskThreeTests {
+
+    @Test
+    public void testDistancePassed () {
+        Point p1 = new Point(10,36.92);
+        Point p2 = new Point(10,0.92);
+        Assert.assertEquals(p1.distance(p2),36.0);
+
+    }
+
+    @Test
+    public void testDistanceFailed () {
+        Point p1 = new Point(-10,36.92);
+        Point p2 = new Point(10,-0.92);
+        Assert.assertEquals(p1.distance(p2),36.0);
+
+    }
+
+    @Test
+    public void testDistanceZero () {
+        Point p1 = new Point(-10,1234567891.99);
+        Point p2 = new Point(-10,1234567891.99);
+        Assert.assertEquals(p1.distance(p2),0.0);
+
+    }
+}
