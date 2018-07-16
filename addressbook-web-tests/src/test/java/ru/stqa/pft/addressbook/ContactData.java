8bcf2ab96a6320463e914fd7248284c3c9e3de27
Daria <37508094+veselkaM@users.noreply.github.com>
2018-07-16 22:31:52 +0300

    Созданы новый тест для создания нового контакта и новый вспомогательный "модельный" класс в пакете ru.stqa.pft.adressbook

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String company;
+    private final String address;
+    private final String mobile;
+    private final String work;
+    private final String email;
+    private final String notes;
+
+    public ContactData(String firstname, String middlename, String lastname, String nickname, String company, String address, String mobile, String work, String email, String notes) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.company = company;
+        this.address = address;
+        this.mobile = mobile;
+        this.work = work;
+        this.email = email;
+        this.notes = notes;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
